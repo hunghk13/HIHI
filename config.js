6ccd47f155979f433640d839cd0f50ea42c1871b
hunghk13 <84701788+hunghk13@users.noreply.github.com>
2021-06-30 20:07:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Continue',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chúng ta lại gặp nhau',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'hihi-iu bạn <3',
+    messDesc: 'Mong sao chúng ta luôn vui vẻ,thân thiết hihi <3 nhớ bạn',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/hungIT.master/' 
+}
